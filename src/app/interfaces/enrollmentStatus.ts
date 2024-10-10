f74ad4fbef21cceff6bafba565feb6ad1eea06a3
mourad <mourad.bouchoucha@djerba.r-iset.tn>
2024-10-10 12:24:42 +0200

    fixing enrollment creation

--- src/app/interfaces/enrollmentStatus.ts
+++ src/app/interfaces/enrollmentStatus.ts
@@ -1,3 +1,3 @@
 export interface EnrollmentStatus {
-    status: 'ALREADY_ENROLLED' | 'PENDING' | 'NOT_ENROLLED'; // Add other possible statuses if needed
+    status: 'APPROVED' | 'PENDING' | 'REJECTED'; // Add other possible statuses if needed
   }
--- src/app/pages/center-front/courses/courses.component.ts
+++ src/app/pages/center-front/courses/courses.component.ts
@@ -10,7 +10,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { DialogServiceService } from 'src/app/services/dialog/dialog-service.service';
 import { NgZone } from '@angular/core';
 import { Location } from '@angular/common';
-import {  ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
+import { ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-courses',
@@ -31,7 +32,7 @@ export class CoursesComponent implements OnInit {
   userData: any = jwtDecode(this.user['accessToken']);
 
   isLoading = false;
-  
+
   constructor(
     private categoryService: CategoryService,
     private coursesService: CourseService,
@@ -57,15 +58,15 @@ export class CoursesComponent implements OnInit {
   }
 
   ngAfterViewInit() {
-    
+
   }
 
-  
+
 
   goBack(): void {
-   // this.location.back();
-   this.showCourses = true;
-   this.showDetails = false
+    // this.location.back();
+    this.showCourses = true;
+    this.showDetails = false
   }
   getUser() {
     let email: any;
@@ -110,82 +111,133 @@ export class CoursesComponent implements OnInit {
   }
 
 
-enroll(courseId: number): void {
-  // Check if the user is logged in
-  if (!localStorage.getItem('token')) {
-    this.dialogService.confirmDialog({
-      title: 'Enrollment',
-      message: 'Please log in to enroll in this course.',
-      cancelText: 'Cancel',
-      confirmText: 'Login'
-    }).subscribe(
-      
+  enroll(courseId: number): void {
+    // Check if the user is logged in
+    if (!localStorage.getItem('token')) {
+      this.dialogService.confirmDialog({
+        title: 'Enrollment',
+        message: 'Please log in to enroll in this course.',
+        cancelText: 'Cancel',
+        confirmText: 'Login'
+      }).subscribe(
+
         () => {
           this.router.navigateByUrl('/login');
         }
-      
-    )
-  } else {
-    // Check the enrollment status for the given course
-    this.enrollmentService.checkEnrollmentStatus(courseId, this.traineeId).subscribe(
-      response => {
-        if (response.status === 'ALREADY_ENROLLED') {
-          this.snackBar.open('You are already enrolled in this course.', 'Close', { duration: 3000 });
-        } else if (response.status === 'PENDING') {
-          this.dialogService.confirmDialog({
-            title: 'Enrollment',
-            message: 'Your enrollment request is pending. Do you want to proceed with enrollment?',
-            cancelText: 'Cancel',
-            confirmText: 'Proceed'
-          }).subscribe(confirmed => {
-            if (confirmed) {
-              // this.enrollmentService.cancelPendingEnrollment(courseId, this.traineeId).subscribe(() => {
-              //   this.createEnrollment(courseId);
-              // });
-            }
-          });
-        } else {
-          // If no enrollment exists, create a new one
-          this.createEnrollment(courseId);
+
+      )
+    } else {
+      // Check the enrollment status for the given course
+      this.enrollmentService.checkEnrollmentStatus(courseId, this.traineeId).subscribe(
+        response => {
+          if (response.status === 'APPROVED') {
+            // 
+            this.dialogService.confirmDialog({
+              title: 'Enrollment',
+              message: 'You have already enrolled in this course.',
+              cancelText: 'OK'
+            }).subscribe(() => {
+              // this.zone.run(() => this.router.navigateByUrl('/'));
+            });
+          } else if (response.status === 'PENDING') {
+            this.dialogService.confirmDialog({
+              title: 'Enrollment',
+              message: 'Your enrollment request is pending. Do you want to proceed with enrollment?',
+              cancelText: 'Cancel',
+              confirmText: 'Proceed'
+            }).subscribe(confirmed => {
+              if (confirmed) {
+                // this.enrollmentService.cancelPendingEnrollment(courseId, this.traineeId).subscribe(() => {
+                //   this.createEnrollment(courseId);
+                // });
+              }
+            });
+          } else {
+            // If no enrollment exists, create a new one
+            this.createEnrollment(courseId);
+          }
+        },
+        error => {
+          this.snackBar.open(`Error checking enrollment status: ${error.message}`, 'Close', { duration: 3000 });
+          console.error('Error checking enrollment status', error);
         }
-      },
-      error => {
-        this.snackBar.open(`Error checking enrollment status: ${error.message}`, 'Close', { duration: 3000 });
-        console.error('Error checking enrollment status', error);
-      }
-    );
-  }
-}
-
-createEnrollment(courseId: number): void {
-  this.isLoading = true;
-  this.enrollmentService.createEnrollment({ courseId, traineeId: this.traineeId }).subscribe(
-    () => {
-      this.isLoading = false;
-      this.snackBar.open('Enrollment successful.', 'Close', { duration: 2000 });
-      // this.dialogService.confirmDialog({
-      //   title: 'Enrollment',
-      //   message: 'You have successfully enrolled in the course.',
-      //   cancelText: 'OK'
-      // }).subscribe(() => {
-      //   this.zone.run(() => this.router.navigateByUrl('/'));
-      // });
-    },
-    error => {
-      this.isLoading = true;
-
-      this.dialogService.confirmDialog({
-        title: 'Enrollment',
-        message: `${error}`,
-        cancelText: 'OK'
-      })
-      
-      console.error('Error during enrollment', error);
+      );
     }
-  );
-}
+  }
 
-  
+  // createEnrollment(courseId: number): void {
+  //   this.isLoading = true;
+  //   this.enrollmentService.createEnrollment({ courseId, traineeId: this.traineeId }).subscribe(
+  //     {
+  //       next: (response) => {
+  //         this.isLoading = false;
+  //         this.snackBar.open('Enrollment successful.', 'Close', { duration: 2000 });
+  //         this.dialogService.confirmDialog({
+  //           title: 'Enrollment',
+  //           message: 'You have successfully enrolled in the course.',
+  //           cancelText: 'OK'
+  //         }).subscribe(() => {
+  //           this.zone.run(() => this.router.navigateByUrl('/'));
+  //         });
+  //       }
+
+  //     },
+  //     // error => {
+  //     //   this.isLoading = false;
+
+  //     //   this.dialogService.confirmDialog({
+  //     //     title: 'Enrollment',
+  //     //     message: `${error}`,
+  //     //     cancelText: 'OK'
+  //     //   })
+
+  //     //   console.error('Error during enrollment', error);
+  //     // }
+
+  //     error: (errorResponse) => {
+  //       this.isLoading = false;
+  //       if (error.status === 409) {
+  //         // Handle conflict error (Trainee already enrolled)
+  //         this.snackBar.open('You have already enrolled in this course.', '', {
+  //           duration: 3000,
+  //         });
+  //       } else {
+  //         // Handle other errors
+  //         this.snackBar.open('An error occurred during enrollment.', '', {
+  //           duration: 3000,
+  //         });
+  //       }
+  //     },
+  //   );
+  // }
+
+
+  createEnrollment(courseId: number) {
+    this.isLoading = true;
+    this.enrollmentService.createEnrollment({courseId,traineeId:this.traineeId}).subscribe({
+      next: (response) => {
+    this.isLoading=false;
+    this.dialogService.confirmDialog({
+      title: 'Enrollment',
+      message: 'You have successfully enrolled in the course.',
+      cancelText: 'OK'
+    }).subscribe(() => {
+      this.zone.run(() => this.router.navigateByUrl('/'));
+    });
+      },
+      error: (errorResponse) => {
+           //   this.isLoading = false;
+        this.dialogService.confirmDialog({
+          title: 'Enrollment',
+          message: `${errorResponse}`,
+          cancelText: 'OK'
+        })
+
+        console.error('Error during enrollment', errorResponse);
+      },
+    });
+  }
+
 
 
 
